ac87bd2122e5228cdfd45085a1854eb79c32f714
xin <48054406+xxfool@users.noreply.github.com>
2025-09-14 17:00:46 +0800

    Update git_test.java

--- internet/src/com/x2space/inetaddress/git_test.java
+++ internet/src/com/x2space/inetaddress/git_test.java
@@ -5,5 +5,8 @@ public class git_test {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello git2");
+        System.out.println("hello git2");
+        System.out.println("hello git2");
     }
 }
