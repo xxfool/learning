7ee21889a37758c783a54a34e404fe41989b5cdc
xin <xuxin@gmail.com>
2025-09-14 16:35:56 +0800

    test-second

--- internet/src/com/x2space/inetaddress/git_test.java
+++ internet/src/com/x2space/inetaddress/git_test.java
@@ -3,5 +3,6 @@ package com.x2space.inetaddress;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
